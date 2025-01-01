7d5ed8813f2e879814f24ea8b2d1f0a1eee01890
Bindalcoder <varunbindal234@gmail.com>
2025-01-01 13:45:55 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,87 @@
+/*
+	Author: BestPixels
+	Template: Home | Single Property HTML Template
+	Version: 1.0
+*/
+	
+"use strict";
+
+/*global jQuery, $*/
+jQuery(document).ready(function(){
+	
+	// Smooth scrolling using jQuery easing
+	jQuery('a.nav-item[href*="#"]:not([href="#"])').on("click", function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+		  var toHash = $(this.hash), toHashN = (this.hash.slice(1)) ? $('[name=' + this.hash.slice(1) + ']') : false;
+		  toHash = toHash.length ? toHash : toHashN;
+		  if (toHash.length) {
+			$('html, body').animate({
+			  scrollTop: (toHash.offset().top - 70)
+			}, 1000, "easeInOutExpo");
+			return false;
+		  }
+		}
+	});
+	
+	// navigation collapse
+	jQuery('.navbar-collapse').on("click", 'li', function() {
+        jQuery('.navbar-collapse').collapse('hide');
+    });
+	
+	// parallax
+	jQuery('.header-section').parallax("50%", 0.1);
+	jQuery('.video-section').parallax("50%", 0.1);
+	jQuery('.cta-section').parallax("50%", 0.1);
+	
+	//magnificPopup	Video
+	jQuery('.embaded-popup').magnificPopup({
+		type: 'iframe',
+		removalDelay: 160,
+		preloader: true,
+		fixedContentPos: false,
+		callbacks: {
+		beforeOpen: function() {
+			  // just a hack that adds mfp-anim class to markup 
+			  this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+			  this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+	});
+	
+	jQuery('.large-view').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside:true,
+		image: {
+			verticalFit: false
+		},			
+		removalDelay: 500, //delay removal by X to allow out-animation
+		callbacks: {
+			beforeOpen: function() {
+		  // just a hack that adds mfp-anim class to markup 
+			this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+			this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.		
+	});	
+	
+	// Item Sliders
+	jQuery('.testimonials-list').slick({
+		dots: true,
+		fade: true,
+		adaptiveHeight: true,
+		autoplay: true,
+		arrows: false,
+	});
+	
+	jQuery('.floor-list').slick({
+		dots: true,
+		adaptiveHeight: true,
+		autoplay: true,
+		arrows: false,
+	});
+	
+	
+});
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# chatgpt page- https://chatgpt.com/c/6768fc51-f8ec-8006-a3d4-8c3b47d837f3
+
+# template link-https://demo.bestpixels.net/home/templates/parallax-background/
+
+# live link
+
+# istock for pics
+
